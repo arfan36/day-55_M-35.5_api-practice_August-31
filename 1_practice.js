366e06ab8597fbe339dd9cfbe960d61e772bdde9
arfan36 <arfanur36@gmail.com>
2022-09-02 17:43:20 +0600

    first commit

---
+++ 1_practice.js
@@ -0,0 +1,33 @@
+/* 
+1. arrow function
+2. template string
+3. forEach (filter, find, map)
+4. API: fetch
+5. DOM er set
+ */
+
+/* 
+TASKS
+1. object with 6 properties. string, boolean, number, function (method, access any object property and return it), array, object
+2. template string: access values from the object above
+    2.1: array third element
+    2.2: property of the object
+3. Arrow function
+    3.1: no parameter return 89
+    3.2: number as parameter and return it by dividing by 17
+    3.3: two parameter: add 7 to the first number and 5 to the second number(in one line) and then add the result and add them and then if the result is even return true otherwise return false
+    3.4: multiline arrow function: array of number as input parameter: sum of all number and then get the square of the result
+4. map
+number of array er each number divide by 7 and return the array
+
+5. difference between map, forEach, filter, find (reduce)
+
+6. challenging:
+declare an object. use destructuring to get an object by excluding one property (hints tut tut tut)
+
+7. JSON placeholder show photos : every album have a button: use modal
+    8.7.1: dynamically display album
+    9.7.2: use arrow function and forEach
+
+10. cocktailDB/audioDB
+ */
